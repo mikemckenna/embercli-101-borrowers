4bdd23a3b3e1d7f0f087aca9664abd01b46c2048
Mike McKenna <junkmckenn@gmail.com>
2016-06-15 11:38:55 -0400

    Added route hooks to reset store/model state

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  resetController: function(controller, isExiting, transition) {
+    if (isExiting) {
+      var model = controller.get('model'); // We grab the model from the controller
+      model.rollback();
+    }
+  }
 });
--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
+  },
+
+  resetController: function(controller, isExiting, transition) {
+    if (isExiting) {
+      var model = controller.get('model');  // We grab the model from the controller
+
+      if (model.get('isNew')) {            // 'isNew': means model wasn't saved to the backend.
+        model.destroyRecord();             // remove model from store
+      }
+    }
   }
 });
